711bf98d15194257a93847cc623f9931872f2e5e
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-19 21:01:47 +0400

    feat: add Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Main from '../Main/Main.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Main />
+          </>
+        }/>
+      </Routes>
     </div>
   );
 }
