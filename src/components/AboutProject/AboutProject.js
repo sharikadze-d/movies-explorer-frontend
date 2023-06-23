8e88568b092776e3b918062c4f612a103b2d15f3
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-23 12:40:33 +0400

    feat: add AboutProject styles + adaptive

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,8 +7,8 @@ export default function AboutProject() {
       <div className='about-project__info'>
         <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
         <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
-        <p className="about__project__item">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className="about__project__item">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <p className="about-project__item">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        <p className="about-project__item">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
       </div>
       <div className="about-project__time-bar">
         <div className="about-project__bar">1 неделя</div>
