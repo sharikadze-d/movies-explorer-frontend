a09ba786f81117cca5fe14798d894f6deccd336d
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-26 23:12:43 +0400

    feat: add AboutMe markup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,3 +1,4 @@
+import AboutMe from '../AboutMe/AboutMe';
 import AboutProject from '../AboutProject/AboutProject';
 import Promo from '../Promo/Promo';
 import Techs from '../Techs/Techs';
@@ -8,6 +9,7 @@ export default function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </>
   )
 }
