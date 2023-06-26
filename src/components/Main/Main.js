11740296e47811cf334c0bdea230f94490de4085
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-26 20:39:44 +0400

    feat: add Techs markup + styles + adaptive

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import AboutProject from '../AboutProject/AboutProject';
 import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
     <>
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   )
 }
