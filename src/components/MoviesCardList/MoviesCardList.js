48c841e2f084d71d2fbfdcdc7bf19e380e01c7c0
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-07-04 22:52:52 +0400

    feat: add MovieCardList adaptive

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -16,12 +16,12 @@ export default function MoviesCardList() {
         <MoviesCard movieData={movieData} />
         <MoviesCard movieData={movieData} />
         <MoviesCard movieData={movieData} />
-        <MoviesCard movieData={movieData} />
-        <MoviesCard movieData={movieData} />
-        <MoviesCard movieData={movieData} />
-        <MoviesCard movieData={movieData} />
-        <MoviesCard movieData={movieData} />
         {/* <MoviesCard movieData={movieData} />
+        <MoviesCard movieData={movieData} />
+        <MoviesCard movieData={movieData} />
+        <MoviesCard movieData={movieData} />
+        <MoviesCard movieData={movieData} />
+        <MoviesCard movieData={movieData} />
         <MoviesCard movieData={movieData} />
         <MoviesCard movieData={movieData} />
         <MoviesCard movieData={movieData} />
