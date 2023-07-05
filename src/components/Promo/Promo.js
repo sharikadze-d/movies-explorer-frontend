9ff07e01db1a148bb9887cc0cc316201aad75f34
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-07-05 21:07:34 +0400

    fix: landing page corrections

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,10 +2,10 @@ import './Promo.css'
 
 export default function Promo() {
   return (
-    <article className='promo'>
-      <div className='promo__inner'>
-        <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
-        <div className='promo__logo'></div>
+    <article className="promo">
+      <div className="promo__inner">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <div className="promo__logo"></div>
       </div>
     </article>
   )
