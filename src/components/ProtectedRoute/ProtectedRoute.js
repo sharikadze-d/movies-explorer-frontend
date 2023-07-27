6657431fd58779c12f3f95bfefcc4e51109e9769
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-07-28 01:20:03 +0400

    fix: исправлен адрес редиректа с защищенных роутов

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,6 +2,6 @@ import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute ({element: Component, ...props}) {
   return (
-    props.isLoggedIn ? <Component {...props} />  : <Navigate to="/signup" replace/>
+    props.isLoggedIn ? <Component {...props} />  : <Navigate to="/" replace/>
   );
 }
